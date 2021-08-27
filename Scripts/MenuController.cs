f86400d0cc76c25dac5922140a31ef91ae53db3c
Vladosbandosss <dasistperfektos@gmail.com>
2021-08-27 12:01:13 +0300

    TaskMenu

---
+++ Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private const string Play = "Game";
+    private const string Menu = "Menu";
+
+    [SerializeField] private GameObject _menuPanel;
+
+   private void Start()
+    {
+        _menuPanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Play);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(Menu);
+    }
+
+    
+}
